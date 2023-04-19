c43d5e62d7797c4e102bc9af5a7451e1d905d516
JJY-sec <wiwiwi79@hanyang.ac.kr>
2023-04-19 23:38:34 +0900

    add compile option for linked object

--- windows_x86_64/compile.sh
+++ windows_x86_64/compile.sh
@@ -7,10 +7,10 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 
 	if x86_64-w64-mingw32-gcc -v 2> /dev/null && x86_64-w64-mingw32-g++ -v 2> /dev/null; then
 		printf "\tCompiling vuln_driver fuzzer...\n"
-		x86_64-w64-mingw32-gcc src/fuzzer/vuln_test.c -I ../ -o bin/fuzzer/vuln_test.exe -lntdll
+		x86_64-w64-mingw32-gcc src/fuzzer/vuln_test.c -I ../ -o bin/fuzzer/vuln_test.exe -lntdll -lpsapi
 	else
 		printf "\tCould not find x86_64-w64-mingw32-gcc/g++! Skipping..\n\t(Try sudo apt install gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64 to fix this)"
-	fi 
+	fi
 else
 	printf "\tError: Need to run Linux to compile these components! Skipping..!\n"
 fi
